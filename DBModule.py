f30d6f71ec17954d575180106b4d6c6a0537382a
bong-u <bongudev@gmail.com>
2022-12-28 12:22:23 +0900

    fix: add cnu notice data

--- DBModule.py
+++ DBModule.py
@@ -6,10 +6,11 @@ class DBModule:
     __sh = __gc.open("cse_notice").sheet1
     
     def read(self):
-        data = [0, 0, 0]
+        data = [0, 0, 0, 0]
         data[0] = self.__sh.get('A2')[0][0]
         data[1] = self.__sh.get('B2')[0][0]
         data[2] = self.__sh.get('C2')[0][0]
+        data[3] = self.__sh.get('D2')[0][0]
 
         return data
     
@@ -17,6 +18,7 @@ class DBModule:
         self.__sh.update_acell('A2', new_data[0])
         self.__sh.update_acell('B2', new_data[1])
         self.__sh.update_acell('C2', new_data[2])
+        self.__sh.update_acell('D2', new_data[3])
 
 if __name__ == '__main__':
     pass
