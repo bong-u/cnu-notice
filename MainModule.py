e95b566103654b66a03fc55e55299ebca1da64ae
bong-u <bongudev@gmail.com>
2022-12-28 11:06:12 +0900

    fix: modify post_list for production

--- MainModule.py
+++ MainModule.py
@@ -12,15 +12,14 @@ class MainModule(DBModule):
     def __init__(self):
         super()
 
-        # recent_post = self.read()
-        recent_post = ['316740', '316519', '316507']
 
         print (datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' | project running...')
 
+        recent_post = self.read()
         post_list = []
-
         cse_crawler = CseCrawler(recent_post)
         post_list = cse_crawler.get_post_list()
+
 
         message_list = self.serialize(post_list)
 
