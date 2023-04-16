2a08755993094eb519ff9874941ee60017a4f8de
bong-u <bongudev@gmail.com>
2023-04-16 13:28:18 +0900

    refactor: rename, remove duplicates

--- CrawlModule.py
+++ CrawlModule.py
@@ -3,44 +3,46 @@ import requests
 
 class CrawlModule():
 
-    @staticmethod
-    def CrawlCNU(recent_post, board_info):
-        new_recent_post = 0
-        post_list = []
-
-        res = requests.get (board_info['url'], headers={'User-Agent':'Mozilla/5.0'})
+    @classmethod
+    def _get_soup(cls, url):
+        res = requests.get(url, headers={'User-Agent':'Mozilla/5.0'})
         res.encoding = 'UTF-8'
-        soup = BeautifulSoup(res.text, 'html.parser')
+        return BeautifulSoup(res.text, 'html.parser')
+
+    @classmethod
+    def crawl_cnu(cls, recent_post, board_info):
+        new_recent_post = 0
+        posts = []
+
+        soup = cls._get_soup(board_info['url'])
 
         for item in soup.select('table > tbody > tr'):
-            L = []
+            cells = []
             for column in item.select('td'):
-                L.append(column.text)
+                cells.append(column.text)
 
-            if L[0] == '공지':
+            if cells[0] == '공지':
                 continue
             if new_recent_post == 0:
-                new_recent_post = L[0]
-            if L[0] == recent_post:
+                new_recent_post = cells[0]
+            if cells[0] == recent_post:
                 break
 
-            post_list.append({
+            posts.append({
                 'channel' : board_info['channel_id'],
-                'title' : L[1],
+                'title' : cells[1],
                 'link' : item.select_one('a')['href'].replace('.', board_info['url_base']),
-                'footer' : L[2]
+                'footer' : cells[2]
             })
 
-        return new_recent_post, reversed(post_list)
+        return new_recent_post, reversed(posts)
     
-    @staticmethod
-    def CrawlCSE(recent_post, board_info):
+    @classmethod
+    def crawl_cse(cls, recent_post, board_info):
         new_recent_post = 0
-        post_list = []
+        posts = []
 
-        res = requests.get (board_info['url'], headers={'User-Agent':'Mozilla/5.0'})
-
-        soup = BeautifulSoup(res.text, 'html.parser')
+        soup = cls._get_soup(board_info['url'])
 
         board = soup.select_one('div.content-wrap tbody')
         for index, notice in enumerate(board.select('tr:not(.b-top-box)')):
@@ -48,18 +50,18 @@ class CrawlModule():
 
             href = element['href']
             title = element.text.strip()
-            post_no = href.split('articleNo=')[1].split('&')[0]
+            post_id = href.split('articleNo=')[1].split('&')[0]
 
             if index == 0:
-                new_recent_post = post_no
-            if post_no >= recent_post:
+                new_recent_post = post_id
+            if post_id >= recent_post:
                 break
 
-            post_list.append({
+            posts.append({
                 'channel' : board_info['channel_id'],
                 'title' : title,
                 'link' : board_info['url'] + href,
                 'footer' : board_info['label']
             })
 
-        return new_recent_post, reversed(post_list)
+        return new_recent_post, reversed(posts)
